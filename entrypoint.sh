933c412ca38153773a6f6fef72fd4a1f95720367
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2021-06-04 03:34:44 +0800

    add container tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 apk update && apk add --no-cache git && apk add --no-cache openssh
 mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
 rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
-cd $MANIFEST_REPO/$SVC_PATH && kustomize edit set image $CONTAINER_REPO:${DRONE_COMMIT_BRANCH}-${DRONE_COMMIT_SHA:0:7}
-git add . && git commit -m "🚀 update to ${DRONE_COMMIT_BRANCH}-${DRONE_COMMIT_SHA:0:7}"
+cd $MANIFEST_REPO/$SVC_PATH && kustomize edit set image $CONTAINER_REPO:$CONTAINER_TAG
+git add . && git commit -m "🚀 update to ${CONTAINER_TAG}"
 git push ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
