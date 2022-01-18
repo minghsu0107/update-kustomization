d739e030e2924630ebac606383ea35feaa521681
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-01-18 16:26:05 +0800

    fix workspace path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@ apk update && apk add --no-cache git && apk add --no-cache openssh
 mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
 rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
 
+cd $MANIFEST_REPO
 git checkout $MANIFEST_BRANCH
-cd $MANIFEST_REPO/$KUSTOMIZATION
+cd $KUSTOMIZATION
 
 for IMAGE in $(echo $IMAGES | sed "s/,/ /g")
 do
